eefb2c80792ec90b23d7407c64e4236ef0a23ad8
Joe Hirst <josephhirst2@gmail.com>
2025-06-05 21:21:12 +0100

    fix typo

--- src/settings.py
+++ src/settings.py
@@ -8,7 +8,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     gemini_api_key: str = Field(alias="GEMINI_API_KEY")
     log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = Field(default="INFO", alias="LOG_LEVEL")
-    script_model: str = Field(alias="GEMINI_SCIRPT_MODEL")
+    script_model: str = Field(alias="GEMINI_SCRIPT_MODEL")
     tts_model: str = Field(alias="GEMINI_TTS_MODEL")
 
     model_config = SettingsConfigDict(env_file=".env")
